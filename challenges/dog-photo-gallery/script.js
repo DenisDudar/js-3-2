dbefd908b44759c61e8991a12861f54d6fdec303
Denis Dudar <denis.dudar11@gmail.com>
2023-10-08 16:58:33 +0300

    Completed the first part of task

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,11 +1,16 @@
 // https://dog.ceo/api/breeds/image/random
 
+<<<<<<< HEAD
 async function randomDogPicture (){
+=======
+async function randomDogPicture() {
+>>>>>>> origin/main
     try {
         const response = await fetch('https://dog.ceo/api/breeds/image/random')
         const json = await response.json()
         return json.message
         
+
     } catch (error) {
         return error
     }
@@ -15,6 +20,7 @@ const button = document.getElementById('btn')
 button.addEventListener('click', async () => {
     const list = document.getElementById('list')
 
+<<<<<<< HEAD
   const item = document.createElement('li')
   list.appendChild(item)
 
@@ -22,4 +28,13 @@ button.addEventListener('click', async () => {
   const pictureUrl = await randomDogPicture()
   picture.src = pictureUrl
   item.appendChild(picture)
+=======
+    const item = document.createElement('li')
+    list.appendChild(item)
+
+    const picture = document.createElement('img')
+    const pictureUrl = await randomDogPicture()
+    picture.src = pictureUrl
+    item.appendChild(picture)
+>>>>>>> origin/main
 })
--- challenges/programmer-humour/script.js
+++ challenges/programmer-humour/script.js
@@ -1,18 +1,19 @@
-async function fetchData(){
+
+async function fetchData() {
    try {
-     const response = await fetch(`https://xkcd.now.sh/?comic=latest`);
-    const data = await response.json()
-    console.log(data)
-    return data.img
+      const response = await fetch(`https://xkcd.now.sh/?comic=latest`);
+      const data = await response.json()
+      console.log(data)
+      return data.img
    } catch (error) {
-    error
+      error
    }
 }
 
-async function createImage(){
-const picture = document.createElement('img')
-picture.src = await fetchData()
-document.body.appendChild(picture)
+async function createImage() {
+   const picture = document.createElement('img')
+   picture.src = await fetchData()
+   document.body.appendChild(picture)
 }
 
 createImage()
